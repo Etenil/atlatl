54b2f049f0d4dbd02deee89e7f389df83d820bae
Etenil <boss@etenil.net>
2012-05-07 12:58:16 +0100

    Added license header.

--- controller.php
+++ controller.php
@@ -4,6 +4,27 @@ namespace Movicon;
 
 require('security.php');
 
+/**
+ * Basic implementation of a Controller.
+ *
+ * Controllers are used to process incoming events. This is the basic
+ * implementation that all controllers should extend. Some helper functions
+ * are provided.
+ *
+ * This file is part of Movicon.
+ *
+ * Movicon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Movicon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Movicon.  If not, see <http://www.gnu.org/licenses/>.
+ */
 class Controller
 {
 	protected $sec;
