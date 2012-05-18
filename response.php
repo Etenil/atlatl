6a135d332d590c7815614989ed665711d8a2fe09
Etenil <boss@etenil.net>
2012-05-18 22:31:31 +0100

    Added HTTP status and content type to responses.

--- response.php
+++ response.php
@@ -22,10 +22,15 @@ namespace atlatl;
 class Response
 {
 	protected $headers;
+	protected $status_code;
 	protected $body;
+	protected $content_type;
 
-	public function __construct($body = '')
+	public function __construct($body = '', $status_code = 200,
+								$content_type = 'text/html; charset=UTF-8')
 	{
+		$this->status_code = $status_code;
+		$this->content_type = $content_type;
 		$this->headers = array();
 		$this->body = $body;
 	}
@@ -54,11 +59,61 @@ class Response
 		$this->headers[$name] = $value;
 	}
 
+	protected function httpStatus()
+	{
+		$statuses = array(
+			100 => '100 Continue',
+			101 => '101 Switching Protocols',
+			200 => '200 OK',
+			201 => '201 Created',
+			202 => '202 Accepted',
+			203 => '203 Non-Authoritative Information',
+			204 => '204 No Content',
+			205 => '205 Reset Content',
+			206 => '206 Partial Content',
+			300 => '300 Multiple Choices',
+			301 => '301 Moved Permanently',
+			302 => '302 Found',
+			303 => '303 See Other',
+			304 => '304 Not Modified',
+			305 => '305 Use Proxy',
+			306 => '306 (Unused)',
+			307 => '307 Temporary Redirect',
+			400 => '400 Bad Request',
+			401 => '401 Unauthorized',
+			402 => '402 Payment Required',
+			403 => '403 Forbidden',
+			404 => '404 Not Found',
+			405 => '405 Method Not Allowed',
+			406 => '406 Not Acceptable',
+			407 => '407 Proxy Authentication Required',
+			408 => '408 Request Timeout',
+			409 => '409 Conflict',
+			410 => '410 Gone',
+			411 => '411 Length Required',
+			412 => '412 Precondition Failed',
+			413 => '413 Request Entity Too Large',
+			414 => '414 Request-URI Too Long',
+			415 => '415 Unsupported Media Type',
+			416 => '416 Requested Range Not Satisfiable',
+			417 => '417 Expectation Failed',
+			500 => '500 Internal Server Error',
+			501 => '501 Not Implemented',
+			502 => '502 Bad Gateway',
+			503 => '503 Service Unavailable',
+			504 => '504 Gateway Timeout',
+			505 => '505 HTTP Version Not Supported'
+			);
+		return $statuses[$this->status_code];
+	}
+	
 	/**
 	 * Generates the page.
 	 */
 	public function compile()
 	{
+		header('HTTP/1.1 ' . $this->httpStatus());
+		header('Content-Type: ' . $this->content_type);
 		foreach($this->headers as $hdrkey => $hdrval) {
 			header($hdrkey, $hdrval);
 		}
