78159ef56de1712408b191c9c68c6c653d403bd3
Guillaume Pasquet <guillaume.pasquet@metropolis.co.uk>
2012-11-20 16:05:50 +0000

    Removed final on modules constructors. Useful when modules are not to be instanciated.

--- src/module.php
+++ src/module.php
@@ -35,7 +35,7 @@ class Module
      * @param array $options is an associative array whose keys will
      * be mapped to properties for speed populating of the object.
 	 */
-	final function __construct(Server $server, $options = NULL)
+	function __construct(Server $server, $options = NULL)
 	{
         $this->server = $server;
         $this->options = $options;
