a13a668b4d805fd74e44857a370023b51efea38f
Guillaume Pasquet <guillaume.pasquet@metropolis.co.uk>
2012-12-03 14:10:25 +0000

    Improved file upload helper with error handling and filename generation.

--- src/request.php
+++ src/request.php
@@ -99,27 +99,12 @@ class Request
 
     function _getAndMoveFile($file, $target, $exts = null)
     {
-        $normalizeChars = array(
-            'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A',
-            'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A',
-            'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I',
-            'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O',
-            'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
-            'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a',
-            'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a',
-            'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i',
-            'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o',
-            'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
-            'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y',
-            'ƒ'=>'f',
-            );
+        $target_filename = basename($target);
+        $target_dir = dirname($target);
+        $target = Utils::joinPaths($target_dir, Utils::cleanFilename($target_filename));
 
-        {
-            $target_filename = basename($target);
-            $target_dir = dirname($target);
-            $target = Utils::joinPaths($target_dir, preg_replace('#[^a-zA-Z0-9._-]#', '_',
-                                                                 strtr($target_filename,
-                                                                       $normalizeChars)));
+        if($file['error'] > 0) {
+            throw new FileUploadError($file['error']);
         }
 
         if($exts != NULL && count($exts > 0)) {
@@ -146,7 +131,7 @@ class Request
     /**
      * Gets an uploaded file.
      */
-    function getFile($slot_name, $target, $exts = NULL)
+    function getFile($slot_name, $target, $exts = NULL, $generate_names = false)
     {
         if(!$target) {
             throw new \Exception("Can't move file without destination.");
@@ -173,13 +158,40 @@ class Request
                     'error' => $_FILES[$slot_name]['error'][$file_num],
                     'size' => $_FILES[$slot_name]['size'][$file_num]
                     );
-                $return[] = $this->_getAndMoveFile($file, Utils::joinPaths($target, basename($file['name'])), $exts);
+
+                $looptarget = '';
+                if($generate_names) {
+                    // Getting the ext. I don't use pathinfo() because the file may not exist...
+                    $ext = substr(strrchr($file['name'], '.'), 0);
+                    $looptarget = Utils::uniqueFilename($target, '', $ext);
+                } else {
+                    $looptarget = Utils::joinPaths($target, basename($file['name']));
+                }
+
+                try {
+                    $return[] = $this->_getAndMoveFile($file, $looptarget, $exts);
+                }
+                catch(FileUploadError $e) {
+                    $return[] = $e;
+                }
             }
         } else {
             if(is_dir($target)) {
-                $target = Utils::joinPaths($target, basename($_FILES[$slot_name]['name']));
+                if($generate_names) {
+                    $ext = substr(strrchr($_FILES[$slot_name]['name'], '.'), 0);
+                    $target = Utils::uniqueFilename($target, '', $ext);
+                } else {
+                    $target = Utils::joinPaths($target, basename($_FILES[$slot_name]['name']));
+                }
             }
-            $return = $this->_getAndMoveFile($_FILES[$slot_name], $target, $exts);
+
+            try {
+                $return = $this->_getAndMoveFile($_FILES[$slot_name], $target, $exts);
+            }
+            catch(FileUploadError $e)
+            {
+                return $e;
+            }
         }
 
         return $return;
---
+++ tests/TestUtils.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once(dirname(__DIR__) . '/src/utils.php');
+
+class TestUtils extends PHPUnit_Framework_TestCase
+{
+    function testJoinPaths()
+    {
+        $this->assertEquals('/thing/stuff/thing2/stuff',
+                            \atlatl\Utils::joinPaths('/thing/stuff', '/thing2/stuff'));
+        $this->assertEquals('/thing/stuff/thing2/stuff',
+                            \atlatl\Utils::joinPaths('/thing/stuff/', '/thing2/stuff'));
+        $this->assertEquals('/thing/stuff/thing2/another stuff',
+                            \atlatl\Utils::joinPaths('/thing/stuff', '/thing2/another stuff'));
+        $this->assertEquals('http://thing/stuff/thing2/stuff',
+                            \atlatl\Utils::joinPaths('http://thing/stuff', '/thing2/stuff'));
+    }
+
+    function testCleanFilename()
+    {
+        $this->assertEquals('abcdefgh', \atlatl\Utils::cleanFilename('abcdefgh'));
+        $this->assertEquals('abcdefgh', \atlatl\Utils::cleanFilename('ábçdéfgh'));
+        $this->assertEquals('abc_defgh', \atlatl\Utils::cleanFilename('abc defgh'));
+        $this->assertEquals('abc_defgh', \atlatl\Utils::cleanFilename('abc$defgh'));
+    }
+
+    function testUniqueFilename()
+    {
+        touch('/tmp/stuff.test');
+        $this->assertNotEquals('/tmp/stuff.test', \atlatl\Utils::uniqueFilename('/tmp', 'stuff', '.test'));
+        $this->assertTrue(file_exists(\atlatl\Utils::uniqueFilename('/tmp', 'stuff', '.test')));
+
+        $filepath = \atlatl\Utils::uniqueFilename('/tmp');
+        $filename = basename($filepath);
+        $this->assertTrue(file_exists('/tmp/' . $filename));
+
+        $filename = basename(\atlatl\Utils::uniqueFilename('/tmp', 'stuff', '.test'));
+        $this->assertRegExp('/^stuff[a-zA-Z0-9]{30}.test$/', $filename);
+
+        $filename = basename(\atlatl\Utils::uniqueFilename('/tmp', 'stuff'));
+        $this->assertRegExp('/^stuff[a-zA-Z0-9]{30}$/', $filename);
+
+        $filename = basename(\atlatl\Utils::uniqueFilename('/tmp', '', '.test'));
+        $this->assertRegExp('/^[a-zA-Z0-9]{30}.test$/', $filename);
+    }
+}
+
+?>
